9f59233225036297f2c0521b809143202fd84006
Carice <145542486+cariceb@users.noreply.github.com>
2024-02-15 11:18:01 +0100

    first commit

---
+++ src/Item.cs
@@ -0,0 +1,7 @@
+// class Kattenvoer
+// {
+// public int Weight { get; }
+// public string Description { get; }
+// public Kattenvoer(int weight, string description)
+//  void kattenvoer() { }
+// }
